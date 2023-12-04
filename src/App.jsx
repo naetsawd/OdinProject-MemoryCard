da8676e9aaee409b1d37bda4e6a22020d1ea15ca
naetsawd <dechsitbkk@gmail.com>
2023-12-04 18:53:38 -0500

    Setup character API fetch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+	const [charList, setCharList] = useState([]);
 
-  return (
-    <>
-      <div>Hello World!</div>
-    </>
-  )
+	useEffect(() => {
+		fetch("https://rickandmortyapi.com/api/character")
+			.then((response) => response.json())
+			.then((data) => {
+				setCharList(data.results.slice(0, 5));
+			})
+			.catch((error) => console.error(error));
+	}, []);
+
+	return (
+		<>
+			<div>
+				{charList.map((char) => (
+					<div>{char.name}</div>
+				))}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
